a6806a57c64da90f378632aff6a4f22c7305e9a1
Ragavan-35 <ragavan6135@gmail.com>
2025-02-12 12:40:51 +0530

    Add files via upload

---
+++ Bitwise.java
@@ -0,0 +1,31 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+	    int a=492;
+	    int b=333;
+	    //int c=20;
+	    int c=a&b;
+	    int d=a|b;
+	    int e=a^b;
+	    
+	       
+	       System.out.println(c);
+	        System.out.println(d);
+	         System.out.println(e);
+	        
+	    
+	       
+	    
+	    
+	    
+		
+	}
+}
