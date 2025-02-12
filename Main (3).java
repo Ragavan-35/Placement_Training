a6df4f54e032d2898378e8434a89936cf54f5b0b
Ragavan-35 <ragavan6135@gmail.com>
2025-02-12 12:29:15 +0530

    Add files via upload

---
+++ Main (3).java
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+	    int a=20;
+	    int b=30;
+	    int c=20;
+	    
+	       int max=(a>b & a>c)?a:(b>c)?b:c;
+	       System.out.println(max);
+	        
+	    
+	       
+	    
+	    
+	    
+		
+	}
+}
