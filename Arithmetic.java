9405a158ca9660884a9a2fa6990f510902057b7e
Ragavan-35 <ragavan6135@gmail.com>
2025-02-12 12:52:49 +0530

    Add files via upload

---
+++ Arithmetic.java
@@ -0,0 +1,41 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+	   
+	   
+	   byte a=100;
+	   byte b=98;
+	   
+	   byte c=(byte)(a+b);
+	   byte d=(byte)(a-b);
+	   byte e=(byte)(a*b);
+	   byte f=(byte)(a/b);
+	   byte g=(byte)(a%b);
+	    
+	    
+	  
+	    
+	       
+	       System.out.println(c);
+	       System.out.println(d);
+	       System.out.println(e);
+	       System.out.println(f);
+	       System.out.println(g);
+	         
+	        
+	    
+	       
+	    
+	    
+	    
+		
+	}
+}
