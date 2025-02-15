45ae2b9f0fb643c98b57a8256044edf95f34f5bf
Ragavan-35 <ragavan6135@gmail.com>
2025-02-15 11:52:03 +0530

    Add files via upload

---
+++ Odd_or_Even.java
@@ -0,0 +1,25 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    //Odd or Even
+	    Scanner scn=new Scanner(System.in);
+	    System.out.println("Enter the Number");
+	    int a=scn.nextInt();
+	    if(a%2==0){
+	        System.out.println(a +""+ "Its Even");
+	    }else System.out.println(a +""+"Its Odd");
+	    
+	    
+	   
+	}
+}
+
