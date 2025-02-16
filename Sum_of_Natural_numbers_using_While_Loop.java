6cb19f12e4fa4d5e0ef3caa55c9d336a232c0388
Ragavan-35 <ragavan6135@gmail.com>
2025-02-16 18:18:41 +0530

    Add files via upload

---
+++ Sum_of_Natural_numbers_using_While_Loop.java
@@ -0,0 +1,30 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    //Sum Of Natural numbers using While Loop
+	    Scanner scan=new Scanner(System.in);
+		int a=scan.nextInt();
+		int b=0;
+	
+		int i=1;
+		while(i<=a){
+		    b=b+i;
+		    i++;
+		    
+		    
+		}
+		  System.out.println("Sum Of Natural Number:"+b);
+		    
+		
+	}
+		
+}
